85ccb26259b0ab45ceada49e895ecf5247b9403e
Tuukka Veteli <tuukka.veteli@solita.fi>
2024-04-07 09:14:09 +0000

    replaced fmt.Println with println

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -38,14 +37,14 @@ func main() {
 	flag.Parse()
 
 	if *isFork && !isGitHubCLIInstalled() {
-		fmt.Println("gh (github-cli) is not installed. Please install it before using the -f flag.")
+		println("gh (github-cli) is not installed. Please install it before using the -f flag.")
 		os.Exit(1)
 	}
 
 	queryIndex := findFlagIndex(args[1:]) + 1
 
 	if !*isFork && len(args) < 2 || *isFork && len(args) < 3 {
-		fmt.Println("\n\nUsage:\nCloning: rlu <search_query>\nForking: Cloning: rlu -f <search_query>\n\n")
+		println("\n\nUsage:\nCloning: rlu <search_query>\nForking: Cloning: rlu -f <search_query>\n\n")
 		os.Exit(1)
 	}
 
@@ -54,7 +53,7 @@ func main() {
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println("Error: ", err)
+		println("Error: ", err)
 		os.Exit(1)
 	}
 
@@ -62,30 +61,30 @@ func main() {
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error: ", err)
+		println("Error: ", err)
 		os.Exit(1)
 	}
 
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		fmt.Println("Unexpected status: ", res.StatusCode)
+		println("Unexpected status: ", res.StatusCode)
 		os.Exit(1)
 	}
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println("Error reading body: ", err)
+		println("Error reading body: ", err)
 		os.Exit(1)
 	}
 
 	var parsedResp QueryResponse
 	if err := json.Unmarshal(body, &parsedResp); err != nil {
-		fmt.Println("Error parsing JSON:", err)
+		println("Error parsing JSON:", err)
 		os.Exit(1)
 	}
 
 	if parsedResp.TotalCount == 0 {
-		fmt.Println("No repositories found")
+		println("No repositories found")
 		os.Exit(0)
 	}
 
